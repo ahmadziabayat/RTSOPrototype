e0e974f169618009d9d5efaa88984548e7b2d064
ahmadziabayat <ahmadzia10@gmail.com>
2019-03-11 21:23:59 -0400

    push

--- src/app/spectrum-display-view/spectrum-display-view.component.ts
+++ src/app/spectrum-display-view/spectrum-display-view.component.ts
@@ -18,11 +18,13 @@ export class SpectrumDisplayViewComponent implements OnInit {
          marker: {color: 'red', symbol:"cross"} },
         { x: [0.3, 10025.225, 20050.15, 30076.075, 40100], y: [2, 5, 3, 6, 0], type: 'scatter', mode: 'lines+markers' },
     ],
-    layout: { height: 252, margin:{l: 30,r: 10,b: 30,t: 0,pad: 4}, autosize: true,
+    layout: { height: 252, margin:{l: 30,r: 10,b: 30,t: 0,pad: 4, autoexpand: true}, autosize: true,
     title: {text:'', xref:'container', yref:'container'}, 
     font: {family:'sans-serif', size: 10},  showlegend: false,
-     xaxis:{title:{text:'Frequency (MHz)'},  autorange:true, rangemode:'tozero', range:[0-12895.647] },
-     yaxis:{title:{text:'Cumulative P'}, autorange:true, rangemode:'nonnegative', fixedrange: true} },
+     xaxis:{title:{text:'Frequency (MHz)'}, 
+     autorange:true, rangemode:'tozero', range:[0-12895.647] },
+     yaxis:{title:{text:'Cumulative P'},
+     autorange:true, rangemode:'nonnegative', fixedrange: true}, plot_bgcolor: '#DEF1F1', },
 
     config: {responsive: true, scrollZoom: true, editable: true, displayModeBar: false,},
     
